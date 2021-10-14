f1064a36b5a3b0a870abc4e9375b5b99ad77264b
omar <39051822+km@users.noreply.github.com>
2021-10-14 13:14:43 +0300

    Added description to OBV

--- Tindicators/Volume.py
+++ Tindicators/Volume.py
@@ -10,6 +10,12 @@ class Volume:
         self.prices = prices
 
     def OBV(self, backtrack = 0):
+        '''
+        backtrack : int
+                  By default it'll get the OBV of the latest day (0) if you want the OBV of the day before then (1) and etc 
+
+        On-Balance Volume
+        '''
         lastprice = self.prices[0]
         obv = 0
         for n in range(1, len(self.prices)-backtrack):
